8d11ef4899396430d70f4ae4eab1d5256761e811
Mikolaj <mkucharski77@gmail.com>
2022-07-28 20:09:10 +0200

    Adding Silly Name Generator App

---
+++ load_dictionary.py
@@ -0,0 +1,12 @@
+import sys
+
+def load(file):
+    """Open a text file & return a list of lowercase strings."""
+    try:
+        with open(file) as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+    except IOError as e:
+        print("{}\n Error opening {}. Terminating program.".format(e, file), file = sys.stderr)
+        sys.exit(1)
